28c523b9a6812cfae651ba425100914a5e1c140a
RyanLouw <110169633+RyanLouw@users.noreply.github.com>
2025-08-29 19:52:13 +0200

    Add missing DI and component imports

--- src/DndGame.Blazor/Program.cs
+++ src/DndGame.Blazor/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using DndGame.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
