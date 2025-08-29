5ab042e1b4c9ff20d7e2e627f491f8d120a95f17
RyanLouw <110169633+RyanLouw@users.noreply.github.com>
2025-08-29 19:37:54 +0200

    Add Blazor client with Firebase login

---
+++ src/DndGame.Blazor/FirebaseAuthService.cs
@@ -0,0 +1,13 @@
+using Microsoft.JSInterop;
+
+namespace DndGame.Blazor;
+
+public class FirebaseAuthService
+{
+    private readonly IJSRuntime _js;
+
+    public FirebaseAuthService(IJSRuntime js) => _js = js;
+
+    public ValueTask<string> SignInWithEmailPassword(string email, string password)
+        => _js.InvokeAsync<string>("firebaseAuth.signIn", email, password);
+}
